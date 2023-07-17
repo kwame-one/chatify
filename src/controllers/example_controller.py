13a2b08ee5c0a3d14a0210d8b9d95836b0e709e9
edwardtsatsu <edwardakorlie73@gmail.com>
2023-07-17 02:38:29 +0000

    suggestion on the router

--- src/controllers/example_controller.py
+++ src/controllers/example_controller.py
@@ -8,26 +8,26 @@ from src.dtos.example_dto import ExampleDto
 from src.requests.example_request import ExampleRequest
 from src.services.example_service import ExampleService
 
-example_router = APIRouter()
+example_router = APIRouter(tags=['Examples'])
 
 
-@example_router.get('', tags=['Examples'], response_model=Page[ExampleDto], status_code=status.HTTP_201_CREATED)
+@example_router.get('', response_model=Page[ExampleDto], status_code=status.HTTP_201_CREATED)
 async def index(service: Annotated[ExampleService, Depends(ExampleService)]):
     return service.find_all()
 
 
-@example_router.get('/{id}', tags=['Examples'])
+@example_router.get('/{id}')
 async def find(id, service: Annotated[ExampleService, Depends(ExampleService)]):
     return service.find(id)
 
 
-@example_router.post('', tags=['Examples'])
+@example_router.post('')
 async def store(example_request: ExampleRequest,
                 service: Annotated[ExampleService, Depends(ExampleService)]) -> ExampleDto:
     return service.store(example_request.model_dump())
 
 
-@example_router.put('/{id}', tags=['Examples'])
+@example_router.put('/{id}')
 async def update(id,
                  example_request: ExampleRequest,
                  service: Annotated[ExampleService, Depends(ExampleService)]) -> ExampleDto:
