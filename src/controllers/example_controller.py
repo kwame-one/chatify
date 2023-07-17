d096c0d477542bb77b841b75bc93b7978382f1f3
edwardtsatsu <edwardakorlie73@gmail.com>
2023-07-17 02:36:13 +0000

    ..

--- src/controllers/example_controller.py
+++ src/controllers/example_controller.py
@@ -11,29 +11,29 @@ from src.services.example_service import ExampleService
 example_router = APIRouter()
 
 
-@example_router.get('', tags=['Examples'], response_model=Page[ExampleDto], status_code=status.HTTP_201_CREATED)
+@example_router.get('',tags=['Examples'], response_model=Page[ExampleDto], status_code=status.HTTP_201_CREATED)
 async def index(service: Annotated[ExampleService, Depends(ExampleService)]):
     return service.find_all()
 
 
-@example_router.get('/{id}', tags=['Examples'])
+@example_router.get('/{id}',tags=['Examples'])
 async def find(id, service: Annotated[ExampleService, Depends(ExampleService)]):
     return service.find(id)
 
 
-@example_router.post('', tags=['Examples'])
+@example_router.post('',tags=['Examples'])
 async def store(example_request: ExampleRequest,
                 service: Annotated[ExampleService, Depends(ExampleService)]) -> ExampleDto:
     return service.store(example_request.model_dump())
 
 
-@example_router.put('/{id}', tags=['Examples'])
+@example_router.put('/{id}',tags=['Examples'])
 async def update(id,
                  example_request: ExampleRequest,
                  service: Annotated[ExampleService, Depends(ExampleService)]) -> ExampleDto:
     return service.update(id, example_request.model_dump())
 
 
-@example_router.delete('/{id}', tags=['Examples'], status_code=status.HTTP_204_NO_CONTENT)
+@example_router.delete('/{id}',tags=['Examples'], status_code=status.HTTP_204_NO_CONTENT)
 async def destroy(id, service: Annotated[ExampleService, Depends(ExampleService)]) -> None:
     return service.delete(id)
